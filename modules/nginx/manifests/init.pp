81a2ce0b57628c3e5dbaee2ba630955e0ac8601c
pbhart <paulbhart@gmail.com>
2014-01-15 13:34:17 -0800

    inital commit

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,37 @@
+# Manage nginx webserver
+class nginx {
+  package { 'apache2.2-common': ensure => absent, }
+
+  package { 'nginx':
+    ensure  => installed,
+    require => Package['apache2.2-common'],
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-enabled/default':
+    source => 'puppet:///modules/nginx/cat-pictures.conf',
+    notify => Service['nginx'],
+  }
+
+  file { '/var/www':
+    ensure => "directory",
+    mode   => 755,
+  }
+
+  file { '/var/www/cat-pictures':
+    ensure  => "directory",
+    mode    => 755,
+    require => File['/var/www'],
+  }
+
+  file { '/var/www/cat-pictures/index.html':
+    source  => 'puppet:///modules/nginx/index.html',
+    require => File['/var/www/cat-pictures'],
+  }
+
+}
+
